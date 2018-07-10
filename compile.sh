e471041558ac085065c77444fd19c65898eb0475
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-07-10 22:52:04 +0200

    Local Version

--- compile.sh
+++ compile.sh
@@ -6,7 +6,3 @@ make distclean
 make isvp_t20_sfcnor_ddr128M_config
 make
 make env
-
-ftp-upload -h ${HOST} -u root --password ismart12 -d /system/sdcard/ u-boot-with-spl.bin
-ftp-upload -h ${HOST} -u root --password ismart12 -d /system/sdcard/ flash.sh
-ftp-upload -h ${HOST} -u root --password ismart12 -d /system/sdcard/ uEnv.sh

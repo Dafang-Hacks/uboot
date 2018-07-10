06628f5923de9685f99ea319e001a430ad2cdc56
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-07-10 22:31:40 +0200

    Version Variable

--- include/configs/isvp_t20.h
+++ include/configs/isvp_t20.h
@@ -127,6 +127,7 @@
 	#define CONFIG_BOOTARGS "console=ttyS1,115200n8 mem=104M@0x0 ispmem=8M@0x6800000 rmem=16M@0x7000000 init=/linuxrc rootfstype=squashfs root=/dev/mtdblock2 rw mtdparts=jz_sfc:256k(boot),2048k(kernel),3392k(root),640k(driver),4736k(appfs),2048k(backupk),640k(backupd),2048k(backupa),256k(config),256k(para),-(flag)"
 #endif
 
+
 /**
  * Boot command definitions.
  */
@@ -137,6 +138,8 @@
 #endif
 
 #define CONFIG_SYS_PROMPT	"U-Boot> "
+#define CONFIG_VERSION_VARIABLE	1
+
 
 #ifdef CONFIG_SFC_NOR
 

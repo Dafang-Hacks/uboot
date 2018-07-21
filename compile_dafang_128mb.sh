3f2026559bd2a1fe98e11ec352606b74da62a4a2
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-07-21 17:41:27 +0200

    Dafang

--- compile_dafang_128mb.sh
+++ compile_dafang_128mb.sh
@@ -3,6 +3,6 @@ TOOLCHAIN=$(pwd)/../toolchain/bin
 export PATH=$TOOLCHAIN:$PATH
 
 make distclean
-make isvp_t20_sfcnor_config
+make isvp_t20_sfcnor_ddr128M_config
 make
-cp u-boot-with-spl.bin opensource-T20-V1.3-64MB.bin
+cp u-boot-with-spl.bin dafang_128mb_v1.bin
